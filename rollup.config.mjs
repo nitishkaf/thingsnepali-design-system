96fe7c4d9d85380abf0e25095ac5925ca4b17856
Nitish Kafle <kaflenit@gmail.com>
2023-12-03 14:13:54 -0600

    feat: add jest for testing and remove module from package

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from '@rollup/plugin-typescript';
 import postcss from "rollup-plugin-postcss";
 import { dts } from 'rollup-plugin-dts';
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
@@ -20,10 +22,12 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
+      terser(),
     ]
   }, {
     input: "dist/esm/types/index.d.ts",
